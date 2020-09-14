c731857e80dda6513ef478c653d48de254672e1f
phoenix <oldjamey@icloud.com>
2020-09-14 12:06:13 -0500

    Update entrypoint.sh
    changed `ln -s` to `ln -sf` to force symlink creation on container restart.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 /etc/init.d/tor start &&\
 onion="$(cat "/var/lib/tor/hidden_service/hostname")"
 cd /etc/nginx/sites-enabled &&\
-ln -s ../sites-available/hiddenservice.conf &&\
+ln -sf ../sites-available/hiddenservice.conf &&\
 sed -i "s/YOUR_HIDDEN_SERVICE_HERE.onion;/$onion;/" hiddenservice.conf &&\
 nginx -t &&\
 echo -e "\n***** $onion *****\n" &&\
